d92d23fc9c396e8c3ddd1cca9a1dd104acceda02
Tony Nguyen <tony.nguyen@usonialabs.com>
2024-01-29 20:50:09 +0700

    Add bootstrap

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,16 @@ import { createContainer } from './di.config';
 import { graph } from './model-source';
 
 export default function run() {
+    // elements
+    const addNodeBtn = document.getElementById('add-node');
+
     const container = createContainer("sprotty-container");
     const modelSource = container.get<LocalModelSource>(TYPES.ModelSource);
     modelSource.setModel(graph);
+
+    console.log('addNodeBtn: ', addNodeBtn)
+
+
 }
 
 document.addEventListener("DOMContentLoaded", () => run());
